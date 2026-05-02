60c1d9263e11391515df76a5a8ed4a2a23095ea4
Josef Šimánek <josef.simanek@gmail.com>
2026-05-03 01:40:45 +0200

    Remove legacy database artifacts

--- db/migrations/04_add_deps_md5_to_rubygems.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Sequel.migration do
-  change do
-    alter_table :rubygems do
-      add_column :deps_md5, String
-    end
-  end
-end
--- db/migrations/06_add_created_at_to_versions.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Sequel.migration do
-  change do
-    alter_table :versions do
-      add_column :created_at, DateTime
-    end
-  end
-end
